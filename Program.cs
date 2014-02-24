5f54447eecdd3c93db1389a5c74f1951ecb16836
Dave Perry <dave@slugburn.com>
2014-02-24 08:53:12 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appDomain = AppDomain.CreateDomain("TestDomain");
+            var creator = (ScenarioInfoCreator)appDomain.CreateInstanceAndUnwrap("AppDomainTest", "AppDomainTest.ScenarioInfoCreator");
+            creator.Create("TestSolver",1);
+            AppDomain.Unload(appDomain);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ScenarioInfoCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using PPA.ScenarioData2;
+
+namespace AppDomainTest
+{
+    public class ScenarioInfoCreator : MarshalByRefObject
+    {
+        public void Create(string solverName, int scenarioId)
+        {
+            var assembly = Assembly.LoadFrom(string.Format(@"..\..\..\TestSolver\bin\debug\{0}.dll", solverName));
+            var solver = (IScenarioSolver) assembly.CreateInstance("TestSolver.MySolver");
+            Console.WriteLine("Creating scenario {0} in AppDomain {1}", scenarioId, AppDomain.CurrentDomain.FriendlyName);
+            var info = new ScenarioInfo();
+            foreach (var aug in solver.GetAugmenters())
+            {
+                aug.Augment(info, null);
+            }
+            Console.WriteLine(info.Properties.Stuff);
+        }
+    }
+}
